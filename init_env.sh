c07fd55391c8ee94ad4409e208d61bd95cc0612b
Jonas Lederer <jonas.lederer@mail.de>
2026-06-09 12:07:47 +0200

    only condaforge channel

--- init_env.sh
+++ init_env.sh
@@ -1,4 +1,4 @@
-export BASE_DIR=/home/lederer/src/MuMo
-export DATA_DIR=/home/lederer/src/MuMo/data
+export BASE_DIR=/l/home/lederer/multimodal_models/MuMo
+export DATA_DIR=$WORKDIR/data
 export HF_HOME=${DATA_DIR}/hf_cache
 export PYTHONPATH=${BASE_DIR}:${PYTHONPATH}
